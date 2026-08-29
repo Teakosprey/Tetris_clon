d1b0933678ce0d13d068f9c6fd3ece0d26db0e59
Teakosprey <said200318@outlook.com>
2026-08-28 19:57:12 -0600

    update - theme_music add

---
+++ audio/music.py
@@ -0,0 +1,23 @@
+import pygame
+
+from game.config import asset_path
+
+
+class Music:
+    def __init__(self):
+        pygame.mixer.music.load(asset_path('theme.mp3'))
+
+    def play(self):
+        pygame.mixer.music.play(-1)
+
+    def stop(self):
+        pygame.mixer.music.stop()
+
+    def pause(self):
+        pygame.mixer.music.pause()
+
+    def resume(self):
+        pygame.mixer.music.unpause()
+
+    def set_volume(self, volume):
+        pygame.mixer.music.set_volume(volume)
--- main.py
+++ main.py
@@ -2,10 +2,19 @@ import pygame
 
 from game.config import GAME_RES, RES
 from game.game import TetrisGame
+from audio.music import Music
+
+
 
 
 def main() -> None:
     pygame.init()
+    pygame.mixer.init()
+
+    music = Music()
+    music.set_volume(1)
+    music.play()
+
     screen = pygame.display.set_mode(RES)
     game_surface = pygame.Surface(GAME_RES)
 
